6252dca4f9cde197b131b65c862f18cfa736a426
Tooru <tom123463@icloud.com>
2024-08-04 00:05:44 +0900

    ver 1.0

--- index.js
+++ index.js
@@ -1,70 +1,177 @@
+//物件
 let result = document.querySelector("#result");
-let process = document.querySelector("#process");
+let prevnum = document.querySelector("#prevnum");
+let operate = document.querySelector("#operate");
+let currentnum = document.querySelector("#currentnum");
+
 let numbers = document.querySelectorAll(".nums");
 let operation = document.querySelectorAll(".opeation");
 
-//functions
-
-let processData = [0]
-let currentOperand;
-let currentAns = 0;
-
-
-function clearProcess(){
-    return processData = [0];
-}
-
-function inputDate(input){
-    processData.push(input);
-    return processData;
-}
-function updateProcess(){
-    process.innerHTML = parseFloat(processData.join("")).toLocaleString();
-}
-
-function updateResult(){
-    result.innerHTML = currentAns;
-    clearProcess();
-}
+//變數
+let prevNumber = " ";
+let currentOp = " ";
+let currentNumber = " ";
+let storage = []; 
 
 
+//功能 
+function update(){
 
+    currentNumber = arrayToNum(storage);
+    if(storage.length === 0) {currentNumber = 0 };
+
+    prevnum.innerHTML = prevNumber;
+    operate.innerHTML = currentOp;
+    currentnum.innerHTML = currentNumber;
+    result.innerHTML = prevNumber;
+};
+
+function arrayToNum(arr){
+    return parseFloat(arr.join(""))
+};
+
+
+
+function calculate(prevNum,operand,currentNum){
+    console.log(prevNumber, currentNumber);
+    prevNum = parseFloat(prevNum);
+    currentNum = parseFloat(currentNum);
+    switch (operand) {
+        case "÷":
+            prevNumber = prevNum / currentNum;
+            break;
+        case "*":
+            prevNumber = prevNum * currentNum;
+            break;
+        case "-":
+            prevNumber = prevNum - currentNum;
+            break;
+        case "+":
+            prevNumber = prevNum + currentNum;
+            break;            
+    }
+    storage = [];
+};
+
+//監聽
 numbers.forEach(item => {
-    item.addEventListener('click',() => {
-        if(item.innerHTML === "." && processData.includes(".")) return;
-        if(processData.length > 12) return
-        else{
-        inputDate(item.innerHTML);
-        updateProcess();
-        console.log(item.innerHTML + "clicked");
-        console.log(processData);
-        }
+    item.addEventListener('click', ()=>{
+        storage.push(item.innerHTML);
+        update();
     })
-});
+})
 
 operation.forEach(item => {
     item.addEventListener('click', ()=>{
-        switch (item.innerHTML){
-            case "DEL" :
-                processData.pop();
-                updateProcess();
-            break
-
-            case "AC" :
-                processData=[0];
-                updateProcess();
-            break
+        console.log(item.innerHTML);
+        switch (item.innerHTML) {
+            case "DEL":
+                storage.pop(1);
+            break;
+            case "AC":
+                prevNumber = " ";
+                currentOp = " ";
+                currentNumber = " ";
+                storage = []; 
+            break;
             
-            case "+/-" :
-                if(processData[0] === "-"){
-                    processData.shift()
-                    updateProcess();
+            case "+/-":
+                if(storage.length === 0) return
+                if(storage[0] === "-") {
+                    storage.shift(1);
                 }else{
-                    processData.unshift("-")
-                    updateProcess();
+                    storage.unshift("-")
                 }
             break
-           
+
+            case "÷" :
+                
+                if(storage.length === 0) {
+                    currentOp = "÷"
+                    update();
+                    return
+                }
+                if(prevNumber === " ") {
+                prevNumber = currentNumber;
+                storage = []; }
+                else{
+                calculate(prevNumber,currentOp,currentNumber);
+                }
+                currentOp = "÷"
+                
+            break
+            case "*" :
+                
+                if(storage.length === 0) {
+                    currentOp = "*"
+                    update();
+                    return
+                }
+                if(prevNumber === " ") {
+                prevNumber = currentNumber;
+                storage = []; }
+                else{
+                calculate(prevNumber,currentOp,currentNumber);
+                }
+                currentOp = "*"
+            break
+            case "-" :
+                
+                if(storage.length === 0) {
+                    currentOp = "-"
+                    update();
+                    return
+                }
+                if(prevNumber === " ") {
+                prevNumber = currentNumber;
+                storage = []; }
+                else{
+                calculate(prevNumber,currentOp,currentNumber);
+                }
+                currentOp = "-"
+            break
+            case "+" :
+                
+                if(storage.length === 0) {
+                    currentOp = "+"
+                    update();
+                    return
+                }
+                if(prevNumber === " ") {
+                prevNumber = currentNumber;
+                storage = []; }
+                else{
+                calculate(prevNumber,currentOp,currentNumber);
+                }
+                currentOp = "+"
+            break
+
+            case "=":
+                if(storage.length === 0) {
+                    currentOp = "+"
+                    update();
+                    return
+                }
+                if(prevNumber === " ") {
+                prevNumber = currentNumber;
+                storage = []; }
+                else{
+                calculate(prevNumber,currentOp,currentNumber);
+                }
+
+                result.innerHTML = parseFloat(prevNumber).toLocaleString();
+                prevnum.innerHTML = " ";
+                operate.innerHTML = " ";
+                currentnum.innerHTML = " ";
+
+                return
+
+            break
+            default:
+                break;
         }
+        update();
     })
-});
+})
+
+//渲染
